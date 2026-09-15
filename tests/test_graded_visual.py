970ea9b259138750f5ffa1f15560bb4e40aa5aa4
Sedat Yıldız <info@sedatyildiz.net>
2026-09-15 16:34:51 +0300

    test: cover raw luminance retinal transduction

--- tests/test_graded_visual.py
+++ tests/test_graded_visual.py
@@ -95,6 +95,15 @@ class GradedVisualTest(unittest.TestCase):
 
         self.assertGreater(float(brain.v[1, 0]), 0.0)
 
+    def test_raw_luminance_path_matches_photoreceptor_shape(self):
+        eyes = Eyes(np.array([-0.5, 0.5], np.float32))
+        drive = eyes.contrast_from_luminance(
+            np.array([0.2, 0.9], np.float32)
+        )
+        self.assertEqual(drive.shape, (2,))
+        self.assertLess(float(drive[0]), 0.0)
+        self.assertAlmostEqual(float(drive[1]), 0.0, places=6)
+
     def test_contrast_transducer_is_signed_and_adapting(self):
         eyes = Eyes(np.array([-0.5, 0.5], np.float32))
 
